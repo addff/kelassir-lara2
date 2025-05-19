1eaf3a90fb0420be25d6790daf72e8b6f2a7cfa3
addff <shahadan@melaka.uitm.edu.my>
2025-05-19 11:15:07 +0800

    Scripting dalam kelas CSP650 pagi isnin

--- kelassir-lara2.bash
+++ kelassir-lara2.bash
@@ -4,14 +4,6 @@
 command -v docker >/dev/null || { echo "Docker tidak dijumpai."; exit 1; }
 command -v docker-compose >/dev/null || { echo "Docker Compose tidak dijumpai."; exit 1; }
 
-# Semak jika Laravel sudah wujud
-if [ -d "kelassir/lara/src/vendor" ]; then
-  echo "Laravel sudah wujud. Langkau create-project."
-else
-  echo "Menjana projek Laravel..."
-  docker run --rm -v $(pwd)/kelassir/lara/src:/app composer create-project --prefer-dist laravel/laravel=11.* /app
-fi
-
 # Masuk ke direktori kelassir
 cd kelassir
 
@@ -24,10 +16,7 @@ while ! docker exec kelassir-lara-app php -r "echo 'ready';" 2>/dev/null; do
   sleep 1
 done
 
-# Jana APP_KEY
-echo "Menjana APP_KEY..."
-docker exec kelassir-lara-app php artisan key:generate
+# Run composer di dalam container
+echo "Menjalankan Composer di dalam container"
+docker-compose exec kelassir-lara-app composer create-project laravel/laravel .
 
-# Jalankan migrasi
-echo "Menjalankan migrasi..."
-docker exec kelassir-lara-app php artisan migrate
